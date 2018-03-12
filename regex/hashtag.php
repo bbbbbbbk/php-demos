f10cc240eec8b41853b1ac1480d55cad681ed8f9
baoerkang <baoerkang@2144.cn>
2018-03-12 14:29:38 +0800

    增加一个匹配中文，字母，数字的表达式

--- regex/hashtag.php
+++ regex/hashtag.php
@@ -1,5 +1,33 @@
 <?php
-$str = "文本文本hastag#hastag##测试tag#测试文本测试文本，#非常厉害#";
-$regex = "/#[^#]+#/";
-preg_match_all($regex, $str, $res);
-print_r($res);
+$str = "文本文本hastag#hastag##测试tag#测试文本测试文本，#非常，厉害#，#厉害了，我的国#";
+//匹配所有
+$tags1 = "/#[^#]+#/";
+//只匹配中文数字和字符
+$tags2 = "/#[^#][\x{4e00}-\x{9fa5}A-Za-z0-9]+#/u";
+preg_match_all($tags1, $str, $res1);
+print_r($res1);
+preg_match_all($tags2, $str, $res2);
+print_r($res2);
+
+/**
+Array
+(
+    [0] => Array
+        (
+            [0] => #hastag#
+            [1] => #测试tag#
+            [2] => #非常，厉害#
+            [3] => #厉害了，我的国#
+        )
+
+)
+Array
+(
+    [0] => Array
+        (
+            [0] => #hastag#
+            [1] => #测试tag#
+        )
+
+)
+*/
